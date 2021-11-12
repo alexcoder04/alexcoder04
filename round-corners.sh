f84d4de78b1e92f293e1314db782e0300d87e5c7
alexcoder04 <ailyushonak@gmail.com>
2021-11-12 18:18:28 +0100

    Revert "update image"
    This reverts commit 192cc84d8d4b60d7505230c16a68a96db126c6b4.

--- round-corners.sh
+++
@@ -1,7 +0,0 @@
-# from https://legacy.imagemagick.org/Usage/thumbnails/#rounded
-convert "$1" \
-  \( +clone  -alpha extract \
-    -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \
-    \( +clone -flip \) -compose Multiply -composite \
-    \( +clone -flop \) -compose Multiply -composite \
-  \) -alpha off -compose CopyOpacity -composite "$2"
